bb105d72c2f7901da1bb9bea6c25799220d73995
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-17 23:21:08 +0100

    Sizeof command

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-/* main - entry point*/
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	
+	printf("Size of char: %d byte(s)", sizeof(char));
+	printf("Size of int: %d byte(s)", sizeof(int));
+	printf("Size of long int: %d byte(s)", sizeof(long int));
+	printf("Siz of long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of float: %d byte(s)", sizeof(float));
+	return (0);
 }
