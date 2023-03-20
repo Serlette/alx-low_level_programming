712ca70c67d5723157594b629409bf74373966da
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-20 23:18:26 +0100

    Using if and else statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c = c++;
+	putchar('\n');
+	}
+	return (0);
+}
