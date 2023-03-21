9a53546dd544d28cb37b0ed5c614b054dad97a67
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-21 04:10:04 +0100

    Check

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/* 101-quote*/
+
+#include <stdio.h>
+
+/**
+ * main - make a quote
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
