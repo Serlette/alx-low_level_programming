453b83f71ce9c4fec1e81a0dbf78c5f66c5b36ea
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-21 04:44:16 +0100

    Space added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,19 @@
 /* 101-quote*/
 
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - make a quote
- * Return: Always 0
+ * Return: Always 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	const char *quote =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	FILE *stream = stderr;
+
+	fwrite(quote, sizeof(char), strlen(quote), stream);
+
 	return (1);
 }
