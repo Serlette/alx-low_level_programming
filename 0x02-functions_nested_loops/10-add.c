fca6a1ef03f7ef0f99d09272182b3e18ab577a34
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-22 05:20:05 +0100

    Tasks 7 to 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a: an integer argument
+ * @b: a second int arg
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
