bb15aedeef48f92ab4a40a21a265a482545c7f12
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-22 03:55:59 +0100

    Task 1 to 6

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ * return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j = 'a';
+
+	while (i < 10)
+	{
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an integer
+ * @int: type of argument
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+
+	return (0);
+}
