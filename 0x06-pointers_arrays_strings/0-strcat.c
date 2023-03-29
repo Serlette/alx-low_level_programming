9d403715680fedca777d65e64634ff87e7b62a9b
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-03-29 18:06:58 +0100

    Concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: a pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+	int p;
+
+	n = 0;
+	m = 0;
+
+	while (dest[n] != '\0')
+		n++;
+
+	while (src[m] != m && m < p)
+	{
+		dest[n] = src[m];
+		i++;
+		j++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,10 @@
+#More on Pointers, Arrays and Strings
+   
+* concatenation of strings
+* Copying a string
+* Comparing two strings
+* Reversing the contents of an array of integers
+* Converting all lowercase to uppercase letters
+* Capitalize all words of a string
+* A function that encodes a string into 1337
+* Other advanced tasks
