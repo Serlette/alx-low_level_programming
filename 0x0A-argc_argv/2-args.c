69d3fca29f225b35e831a8a8c9d05919f8e5ad86
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-04-11 05:57:33 +0100

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
+
+/**
+ * main - progrsm function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = atoi[char argv[i];
+			mul = mul * atoi(char argv[i]);
+				   }
+		}
+}
