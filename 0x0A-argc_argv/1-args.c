f3ec8d1a04bf88bfab1ad0db05dafea408aae252
Ser Lette <108860422+Serlette@users.noreply.github.com>
2023-04-11 05:34:24 +0100

    Corrections

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,7 +13,11 @@ int main(int argc, char *argv[] __attribute__ ((unused)))
 {
 	int i;
 
-	for (i = 1; i < argc; i++)
-		printf("%d\n", argc);
+	i = 0;
+
+	while (i < argc)
+		i++;
+	printf("%d\n", i);
+
 	return (0);
 }
